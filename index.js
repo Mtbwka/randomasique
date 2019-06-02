ef6ee16487e4709be077fda11c2cfe31aec5f3ab
Viktor <meow.lululu@gmail.com>
2019-06-02 02:29:46 +0200

    Init commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const TelegramBot = require('node-telegram-bot-api');
+const token = process.env.BOT_API_KEY;
+const bot = new TelegramBot(token, {
+  polling: true
+});
+
+const random = max => {
+  return Math.floor(Math.random() * (max - 0));
+};
+
+const randomCollection = () => {
+  const photoCollections = [0, 100, 200];
+  return photoCollections[Math.floor(Math.random() * photoCollections.length)];
+};
+
+bot.onText(/\paehali/, msg => {
+  const chatId = msg.chat.id;
+  const user_profile = bot.getUserProfilePhotos(
+    process.env.GALLERY,
+    randomCollection()
+  );
+
+  user_profile.then(res => {
+    const file_id = res.photos[random(res.photos.length)][0].file_id;
+    const file = bot.getFile(file_id);
+    file.then(result => {
+      bot.sendPhoto(chatId, result.file_id);
+    });
+  });
+});
